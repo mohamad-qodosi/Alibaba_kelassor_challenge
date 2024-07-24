eec34c2e60d6fa3c3f19391fe295ae0a09c267c2
taha <tahatahmasbypour@gmail.com>
2024-07-24 12:07:32 +0330

    new_train

--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 class train:
     def __init__(self,do,date):
-        pass
+        self.do  = do 
+        self.date = date
     def valid(self):
-        pass
+        if self.do == "AB" or self.do == "BA" or self.do == "BC" or self.do == "CB" or self.do == "DC" or self.do == "CD" :
+            return True
+        else :
+            return False
