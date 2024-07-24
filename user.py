7962322fee54e52fe795e40d6833b491a143fb84
parsamhb82 <parsa1382mohebian@gmail.com>
2024-07-24 11:39:27 +0330

    initial commit User

---
+++ user.py
@@ -0,0 +1,19 @@
+class User:
+    def __init__(self, user_name: str , password : int) -> None:
+        self.username = user_name
+        self.password = password
+        self.train_tikets = []
+        self.flight_tickets = []
+    def add_train_ticket(self, train : Train): # type: ignore
+        self.train_tikets.append(train)
+    def add_flight_ticket(self, flight: Flight): # type: ignore
+        self.flight_tickets.append(flight)
+    def get_train_tickets(self):
+        return self.train_tikets
+    def get_flight_tickets(self):
+        return self.flight_tickets
+
+
+    
+
+        
